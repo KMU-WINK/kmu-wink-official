023436d0d4c84b8b7cffbd9e73ef2dedf42a7c55
bell2lee <paperlee@kookmin.ac.kr>
2020-07-28 01:58:48 +0900

    models 정의

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from board.models import Board, Document, Comment
 # Register your models here.
+
+admin.site.register(Board)
+admin.site.register(Document)
+admin.site.register(Comment)
