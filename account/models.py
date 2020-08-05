bffa8a54809a3ecfc3aee5bc4c4c89b672fe0118
bell2lee <paperlee@kookmin.ac.kr>
2020-08-05 23:28:58 +0900

    add main page banner & Custom User Manager

--- account/models.py
+++ account/models.py
@@ -2,29 +2,39 @@ from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,Permissi
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, email, nickname, password=None):
+    def create_user(self, email, password, date_of_birth, name, student_number, department, position, graduation_date):
         if not email:
             raise ValueError('must have user email')
         user = self.model(
             email=self.normalize_email(email),
-            nickname=nickname
+            date_of_birth=date_of_birth,
+            name=name,
+            student_number=student_number,
+            department=department,
+            position=position,
+            graduation_date=graduation_date
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, nickname, password):
+    def create_superuser(self, email, password, date_of_birth, name, student_number, department, position, graduation_date):
+        print(email, password, date_of_birth, name, student_number, department, position, graduation_date);
         user = self.create_user(
             email=self.normalize_email(email),
-            nickname=nickname,
-            password=password
+            password=password,
+            date_of_birth=date_of_birth,
+            name=name,
+            student_number=student_number,
+            department=department,
+            position=position,
+            graduation_date=graduation_date
         )
         user.is_admin = True
         user.is_superuser = True
@@ -51,4 +61,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['date_of_birth']
+    REQUIRED_FIELDS = ['date_of_birth', 'name', 'student_number', 'department', 'position', 'graduation_date']
--- board/views.py
+++ board/views.py
@@ -3,6 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
+# 게시판 메인페이지
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return render(request, 'boards.html')
+
+def board(request, board_name):
+    # board_name으로 게시판 가져오기
+
+    return HttpResponse(board_name)
+
+
+# 메인페이지
+def main_index(request):
+    return render(request, 'index.html')
