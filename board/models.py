6711e62f4818fecaa91182bbf231922adc6c0990
kimgyuri <rosa980309@kookmin.ac.kr>
2020-09-29 03:43:53 +0900

    ckeditor 기능 추가

--- board/models.py
+++ board/models.py
@@ -27,7 +27,7 @@ class Board(models.Model):
 class Document(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE, null=False, verbose_name="게시판")
     title = models.CharField(max_length=100, null=False, verbose_name="게시물 제목")
-    content = RichTextField()
+    content = RichTextField() 
     #content = models.TextField(max_length=5000, null=False, verbose_name="본문")
     make_date = models.DateTimeField(default=now, verbose_name="작성일")
     modifiy_date = models.DateTimeField(null=True, blank=True, verbose_name="최근 수정일")
