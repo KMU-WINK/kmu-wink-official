f8b3adf34b5a88059e41ed6b3464d8b6fd6f4dea
kimgyuri <rosa980309@kookmin.ac.kr>
2020-09-29 04:33:02 +0900

    CKeditor 이미지 파일 업로드 기능 추가

--- wink_official/urls.py
+++ wink_official/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from board.views import main_index
 from django.conf.urls import url
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,7 @@ urlpatterns = [
     path('board/', include('board.urls')),
     path('member/', include('account.urls')),
     url(r'^captcha/', include('captcha.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
